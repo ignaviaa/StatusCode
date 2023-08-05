59161a09c3ba83913ac1988c1f6543b5fe92b378
AbhayShaw01 <blinds8ul@gmail.com>
2023-08-06 02:02:55 +0530

    changed the sideBar a bit

--- carbon-world/src/components/NavItems.tsx
+++ carbon-world/src/components/NavItems.tsx
@@ -6,17 +6,17 @@ import {LuLayoutDashboard} from "react-icons/lu";
 
 function NavItems() {
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-4 ">
 
-            <Link href={"/"}>
+            <Link className="flex justify-center items-center hover:bg-[#4D4D4D] hover:rounded  m-2 gap-2 p-2" href={"/"}>
                 <LuLayoutDashboard/> Dashboard
             </Link>
 
-            <Link href={"/"}>
+            <Link className="flex justify-center items-center hover:bg-[#4D4D4D] hover:rounded  m-2 gap-2 p-2 " href={"/"}>
                 <AiOutlineTransaction/> Transaction
             </Link>
-            <Link href={"/"} >
-                <BiLogOut/> Logout
+            <Link className="flex justify-center items-center m-2  gap-2 hover:bg-[#4D4D4D] hover:rounded  p-2" href={"/"}>
+                <BiLogOut/>Log Out
             </Link>
         </div>
     );
