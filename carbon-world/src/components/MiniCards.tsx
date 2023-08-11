4cb42c44cf61010bc522bbd0c9fd751a2eb98924
AbhayShaw01 <blinds8ul@gmail.com>
2023-08-11 16:47:17 +0530

    added investor route and feature to Cardcomp and minicard created

---
+++ carbon-world/src/components/MiniCards.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function MiniCards() {
+    return (
+        <>
+            <div
+                className="max-w-xs rounded-lg overflow-hidden shadow-lg bg-[#202123]"
+                style={{width: '250px', height: '150px'}}
+            >
+                <div className="px-5 py-4">
+                    <div className="font-bold text-xl mb-2 text-white">The Coldest Sunset</div>
+                    <p className="text-white text-base">
+
+                    </p>
+                </div>
+            </div>
+
+        </>
+
+    );
+}
+
+export default MiniCards;
