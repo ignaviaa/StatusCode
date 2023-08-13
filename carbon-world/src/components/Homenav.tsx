f576b3ba3b17e5a822755d3bab18f653f35fd563
ignaviaa <prerna.staugustine8@gmail.com>
2023-08-13 14:00:37 +0530

    added button

--- carbon-world/src/components/Homenav.tsx
+++ carbon-world/src/components/Homenav.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 function Homenav() {
  return(
     <div className=' sticky font-Poppins bg-white text-black h-10 p-10 flex flex-row items-center'>
-    <div className='font-semibold text-3xl text-green-600'>
+    <div className='font-semibold text-3xl text-[#54b946]'>
      EcoToken
 </div>
-<div className='items-center relative left-[50%] flex flex-row gap-5'>
+<div className='items-center relative left-[60%] flex flex-row gap-6'>
     <p>Our Approach</p>
     <p>Marketplace</p>
+    <button className=' bg-[#54b946] p-3 rounded-lg font-Poppins text-white'>Get Started</button>
     
     </div>
 
